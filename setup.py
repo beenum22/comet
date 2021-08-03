fa77af585135c92e6cadae3231795d735258d2fe
bitbucket-pipelines <commits-noreply@bitbucket.org>
2021-08-03 10:06:05 +0000

    chore: update comet config and project version files for .
    [skip ci]

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ pkgs, links = get_requirements()
 
 setup(
     name='comet',
-    version='0.2.0-dev.4',
+    version='0.2.0-dev.5',
     license='LICENSE',
     description='Comet is a simple tool to automate/facilitate automated release cycle.',
     long_description=readme(),
--- src/comet/cli.py
+++ src/comet/cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'Muneeb Ahmad'
-__version__ = '0.2.0-dev.4'
+__version__ = '0.2.0-dev.5'
 
 import sys
 import os
